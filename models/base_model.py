9c9c5fcff19c0cb9780cc19cdf2b25dc7a31e459
RaphSchp <raphaelschopfer@gmail.com>
2023-11-02 13:21:13 +0000

    add base_model.py + __init__.py + AUTHORS

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Base Model"""
+import uuid
+import models
+from datetime import datetime
+
+
+class BaseModel:
+    """Base class for the console clone"""
+    def __init__(self, *args, **kwargs):
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                        )
+                    setattr(self, key, value)
+        else:
+            # Add object to storage file
+            models.storage.new(self)
+
+    def __str__(self):
+        """Reformating string repr"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """Updating the update_at attr with current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Making a dictionary repr of this instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
